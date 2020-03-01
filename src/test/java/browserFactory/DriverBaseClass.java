a059d14a257fc49fde523254ea3923b54f80ba1e
Lalit <lalittew@gmail.com>
2020-03-01 00:51:58 +0000

    commit before and after to check that before and after annotation is working or not ?

--- src/test/java/browserFactory/DriverBaseClass.java
+++ src/test/java/browserFactory/DriverBaseClass.java
@@ -1,7 +1,11 @@
 package browserFactory;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
 
 public class DriverBaseClass {
     public static WebDriver driver;
@@ -15,5 +19,21 @@ public class DriverBaseClass {
         driver.get("https://www.myprotein.com/");
 
         return driver;
+    }
+
+    @Before
+    public void startTest()
+    {
+        System.out.println("**********************");
+        System.out.println("Test Started");
+
+    }
+
+    @After
+    public void afterTest()
+    {
+        System.out.println("**********************");
+        System.out.println("Test Finished");
+
     }
 }
