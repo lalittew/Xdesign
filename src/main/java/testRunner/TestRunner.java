eff47094a71ce7f8885e3f613d60fb733241ad66
Lalit <lalittew@gmail.com>
2020-03-03 02:47:16 +0000

    Updated test runner class to get the html reports

--- src/main/java/testRunner/TestRunner.java
+++ src/main/java/testRunner/TestRunner.java
@@ -10,7 +10,9 @@ import io.cucumber.junit.Cucumber;
         strict = true,
         features = "./Features",
         glue = "stepDefinitions",
-        tags = "@test"
+        tags = "@test",
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        monochrome = true
 )
 
 public class TestRunner {
