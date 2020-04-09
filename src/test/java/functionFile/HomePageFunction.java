fadeecd59658b5d4fceb2aeac25d53c4d3378187
Lalit <lalittew@gmail.com>
2020-04-09 19:49:47 +0100

    Added new test for valid credentials

--- src/test/java/functionFile/HomePageFunction.java
+++ src/test/java/functionFile/HomePageFunction.java
@@ -11,4 +11,6 @@ public class HomePageFunction extends DriverBaseClass {
     @FindBy(how = How.LINK_TEXT, using = "Account")
     public WebElement accountOption;
 
+    @FindBy(how = How.XPATH, using = "//*[@id=\"account\"]/div[3]/div/div[1]/div[1]/p")
+    public WebElement welcomeText;
 }
--- src/test/java/stepDefinitions/HomePage.java
+++ src/test/java/stepDefinitions/HomePage.java
@@ -7,6 +7,7 @@ import functionFile.LoginPageFunction;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
@@ -41,6 +42,22 @@ public class HomePage extends DriverBaseClass {
         generalFunctions.checkElementPresent(loginPageFunction.emailAddressTextBox);
         generalFunctions.checkElementPresent(loginPageFunction.passwordTextBox);
     }
+
+    @Then("I enter valid {string} and {string} and click Login")
+    public void i_enter_valid_and_and_click_Login(String email, String password) throws InterruptedException {
+        generalFunctions.enterText(loginPageFunction.emailAddressTextBox,email);
+        generalFunctions.enterText(loginPageFunction.passwordTextBox,password);
+        generalFunctions.click(loginPageFunction.signInButton);
+        Thread.sleep(500);
+    }
+
+    @Then("I can see my {string} on account page")
+    public void i_can_see_my_on_account_page(String name) {
+        String expectedWelcomeText = homePageFunction.welcomeText.getText();
+        String actualWelcomeText = "Welcome "+name;
+        Assert.assertEquals("Expected welcome text is not equal to Actual welcome text", expectedWelcomeText, actualWelcomeText);
+    }
+
 
     @Then("I enter wrong {string} and {string} and click Login")
     public void i_enter_wrong_and_and_click_Login(String username, String password) throws InterruptedException {
